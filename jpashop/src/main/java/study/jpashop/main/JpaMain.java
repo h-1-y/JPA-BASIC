a29b3556bebadec875c46c1f89e76f92d4d9597f
h-1-y <h-1-y@naver.com>
2024-07-18 18:44:38 +0900

    entity 생

---
+++ jpashop/src/main/java/study/jpashop/main/JpaMain.java
@@ -0,0 +1,38 @@
+package study.jpashop.main;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+	public static void main(String[] args) {
+		
+		// EntityManagerFactory는 하나만 생성해서 애플리케이션 전체에서 공유 
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		// EntityManager는 스레드간 공유 X(사용하고 버려야 함)
+		EntityManager em = emf.createEntityManager();
+		
+		EntityTransaction et = em.getTransaction();
+		
+		et.begin();
+		
+		try {
+			
+			et.commit();
+			
+		} catch (Exception e) {
+			
+			et.rollback();
+			
+		} finally {
+			
+			em.close();
+			emf.close();
+			
+		}
+		
+	}
+	
+}
